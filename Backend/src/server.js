ffff7fc7792b6d1a9844ec9adf4df1c6c5536e7d
Aryan9060 <Aryanofficial9060@gmail.com>
2026-03-05 10:09:43 +0530

    feat: integrate mongoose for MongoDB support; add database connection and user model

---
+++ Backend/src/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+console.log(process.env.CORS_ORIGIN);
+// dotenv.config();
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, () => {
+  //   console.log(`Server is running on port ${port}`);
+// });
+
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// Import other routes
+import userRouter from "./routes/user.routes.js";
+
+//route diclarations
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:5000/api/v1/users
+export { app };
